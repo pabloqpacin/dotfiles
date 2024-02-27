30405f579f2ad6d1141351042d68f060ef5ec327
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2024-02-27 02:55:24 +0100

    Update UbuntuServer-base

--- .zshrc
+++ .zshrc
@@ -53,6 +53,12 @@ fi
 source $ZSH/oh-my-zsh.sh
 
 
+# PAGERZ
+export DELTA_PAGER='less -j2 -FR --mouse --wheel-lines 4'
+# export BAT_PAGER='less -j2 -FR --mouse --wheel-lines 4'
+  # $ delta --help | grep -C3 pager
+
+
 # NVM installation requirements -- auto-written after curl
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
--- scripts/autosetup/UbuntuServer-base.sh
+++ scripts/autosetup/UbuntuServer-base.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo -e "\n----################################################################----"
-echo -e   "#######~~~~{     UbuntuServer-base v1.0  by @pabloqpacin    }~~~~#######"
+echo -e   "#######~~~~{     UbuntuServer-base v1.1  by @pabloqpacin    }~~~~#######"
 echo -e "----################################################################----\n"
 
-# TO DO BEFOREHAND: sudo su && passwd --> to login directly as root :D
+### Tested successfully on Ubuntu 22.04 (fresh VM):
+# wget https://raw.githubusercontent.com/pabloqpacin/dotfiles/main/scripts/autosetup/UbuntuServer-base.sh && \
+# bash UbuntuServer-base.sh
+
+### NOTE: run as normal user, not tested as root
 
 ########## VARIABLES ##########
 
@@ -30,20 +34,13 @@ update_system() {
         sudo apt-get autoclean -y
 }
 
-
 install_base_apt() {
 
-    # if ! sudo apt-get install build-essential -y --simulate 2>&1 | grep -q 'is already the newest version'; then
-    #     $sa_install build-essential && should_reboot=1
-    # fi
-   
-    # $sa_install libssl-dev sysbench
     $sa_install curl git openssh-server wget
     $sa_install neofetch --no-install-recommends
-    $sa_install btop grc ipcalc nmap ripgrep tldr tmux tree 
+    $sa_install btop grc ipcalc mycli nmap ripgrep tldr tmux tree
     $sa_install bat && sudo mv /usr/bin/batcat /usr/bin/bat
     # $sa_install python3-pip python3-venv --no-install-recommends
-    # $sa_install mycli     # default-mysql-client mariadb-client
 
     if [ ! -d ~/.local/share/tldr ]; then
         mkdir -p ~/.local/share &>/dev/null
@@ -51,22 +48,53 @@ install_base_apt() {
     fi
 }
 
+install_base_dpkg(){
+
+    lf_pkg='lf-linux-amd64.tar.gz'
+    wget -q https://github.com/gokcehan/lf/releases/download/r31/$lf_pkg && \
+        tar -zxvf $lf_pkg && \
+        sudo mv lf /usr/bin/lf && \
+        rm $lf_pkg
+
+    delta_pkg='git-delta_0.16.5_amd64.deb'
+    wget -q https://github.com/dandavison/delta/releases/download/0.16.5/$delta_pkg && \
+        sudo dpkg -i $delta_pkg && \
+        rm $delta_pkg
+}
+
 clone_dotfiles() {
     if [ ! -d ~/dotfiles ]; then
         git clone --depth 1 https://github.com/pabloqpacin/dotfiles ~/dotfiles; fi
-    
+
     if [ ! -d ~/.config ]; then
         mkdir ~/.config &>/dev/null; fi
 
-    if [ ! -L ~/.gitconfig ]; then
-        ln -s ~/dotfiles/.gitconfig ~/; fi
+    if [ ! -L ~/.myclirc ]; then
+        ln -s ~/dotfiles/.myclirc ~/; fi
 
-    if [ ! -L ~/.config/btop ]; then
-        ln -s ~/dotfiles/.config/btop ~/.config; fi
+    if [ ! -L ~/.config/bat ]; then
+        ln -s ~/dotfiles/.config/bat ~/.config; fi
+
+    # if [ ! -L ~/.config/btop ]; then
+    #     ln -s ~/dotfiles/.config/btop ~/.config; fi
+
+    if [ ! -L ~/.config/lf ]; then
+        ln -s ~/dotfiles/.config/lf ~/.config; fi
 
     if [ ! -L ~/.config/tmux ]; then
         ln -s ~/dotfiles/.config/tmux ~/.config; fi
 
+    if [ ! -L ~/.vimrc ]; then
+        ln -s ~/dotfiles/.vimrc ~/ && \
+            sudo ln -s $HOME/dotfiles/.vimrc /root/ && \
+            sed -i 's/nvim/vim/g' ~/dotfiles/.zshrc
+    fi
+
+    if [ ! -L ~/.gitconfig ]; then
+        ln -s ~/dotfiles/.gitconfig ~/ && \
+            sed -i '/github/d' ~/.gitignore && \
+            sed -i "s/pabloqpacin/$USER/" ~/.gitignore
+    fi
 }
 
 setup_zsh() {
@@ -77,25 +105,19 @@ setup_zsh() {
         bash $HOME/dotfiles/scripts/setup/omz-msg_random_theme.sh
     fi
     
-    # if [[ $current_shell != 'zsh' ]]; then
-    #     sudo chsh -s $(which zsh) $USER; fi
+    if [[ $current_shell != 'zsh' ]]; then
+        sudo chsh -s $(which zsh) $USER
+    fi
     
     if [ ! -L ~/.zshrc ]; then
         mv ~/.zshrc{,.bak} &&
         ln -s ~/dotfiles/.zshrc ~/
     fi
     
-    if [ ! -d ~/dotfiles/zsh/plugins ]; then
-        mkdir ~/dotfiles/zsh/plugins; fi
-    
-    if [ ! -d ~/dotfiles/zsh/plugins/zsh-autosuggestions ]; then
-        git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $HOME/dotfiles/zsh/plugins/zsh-autosuggestions; fi
-    
-    if [ ! -d ~/dotfiles/zsh/plugins/zsh-syntax-highlighting ]; then
-        git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting $HOME/dotfiles/zsh/plugins/zsh-syntax-highlighting; fi
-
+    if [[! -d ~/dotfiles/zsh/plugins/zsh-autosuggestions || ! -d ~/dotfiles/zsh/plugins/zsh-syntax-highlighting ]]; then
+        bash $HOME/dotfiles/zsh/plugins/clone-em.sh
+    fi
 }
-
 
 install_base_go() {
     $sa_update && $sa_install golang
@@ -105,19 +127,16 @@ install_base_go() {
     if ! command -v fzf &>/dev/null; then
         go install github.com/junegunn/fzf@latest; fi
     
-    if ! command -v lf &>/dev/null; then
-        go install github.com/gokcehan/lf@latest; fi
-
-    if [ ! -L ~/.config/lf ]; then
-        ln -s ~/dotfiles/.config/lf ~/.config; fi
+    # if ! command -v lf &>/dev/null; then
+    #     go install github.com/gokcehan/lf@latest; fi
 
     if ! command -v cheat &>/dev/null; then
         go install github.com/cheat/cheat/cmd/cheat@latest; fi
 
     if [ ! -L ~/.config/cheat/conf.yml ]; then
         yes | cheat
-        bash -x $HOME/dotfiles/scripts/setup/cheat-symlink.sh; fi
-
+        bash -x $HOME/dotfiles/scripts/setup/cheat-symlink.sh
+    fi
 }
 
 install_docker() {
@@ -133,11 +152,39 @@ install_docker() {
             sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         $sa_update && $sa_install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             sudo usermod -aG docker $USER
-            newgrp docker
+            # newgrp docker
     fi
-
 }
 
+
+########### RUNTIME ###########
+
+# echo "$(dpkg -l | wc -l) paquetes intalados" >> /tmp/install.log && \
+# df -h | grep '/$' >> /tmp/install.log
+
+set_variables
+
+update_system
+install_base_apt
+install_base_dpkg       # git-delta lf
+
+clone_dotfiles          # symlinks: bat mycli tmux .vimrc .gitconfig    # btop
+setup_zsh
+
+# install_base_go         # cheat fzf   # lf
+install_docker
+
+echo "" && neofetch && echo -e "\nKindly reboot.\n"
+
+
+# ---
+
+# # Neovim: change onedark for rosepine
+# NVIM="$HOME/.config/nvim"
+# PACKER="$NVIM/lua/pabloqpacin/packer.lua"
+# PLUGINS="$NVIM/after/plugin"
+# nvim_sed_packer
+# nvim_move_colordirs
 
 # nvim_sed_packer() {
 #     sed .config/nvim/lua/pabloqpacin/packer.lua
@@ -148,35 +195,9 @@ install_docker() {
 #     mv $PLUGINS/colors-rosepine.lua.bak $PLUGINS/colors-rosepine.lua
 # }
 
-use_vim() {
 
-    if [ ! -L ~/.vimrc ]; then
-        ln -s ~/dotfiles/.vimrc ~/
-    fi
+# # TO DO BEFOREHAND: sudo su && passwd --> to login directly as root :D
 
-    sed -i 's/nvim/vim/g' ~/.zshrc
-
-}
-
-########### RUNTIME ###########
-
-set_variables
-
-update_system
-install_base_apt
-
-clone_dotfiles      # also symlinks: btop tmux
-setup_zsh
-
-install_base_go
-install_docker
-
-use_vim
-
-# # Neovim: change onedark for rosepine
-# NVIM="$HOME/.config/nvim"
-# PACKER="$NVIM/lua/pabloqpacin/packer.lua"
-# PLUGINS="$NVIM/after/plugin"
-# nvim_sed_packer
-# nvim_move_colordirs
-
+    # # No exa (no nerdfont) in server... but what if ssh? Mm just not that important imho
+    # if command -v exa &>/dev/null; then
+    #     sed -i 's/eza/exa/g' ~/dotfiles/zsh/aliases.zsh; fi
